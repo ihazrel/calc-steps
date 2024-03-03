5cc7eba4134dee9fec2e28d5bc3c9f9c60cbd115
Hazrel Idlan <ihazrel@gmail.com>
2024-03-03 15:16:05 +0800

    added calc

--- calc_steps/lib/util/steps_background.dart
+++ calc_steps/lib/util/steps_background.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class StepsBackground extends StatelessWidget {
   const StepsBackground({super.key});
@@ -7,13 +8,27 @@ class StepsBackground extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 500,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Color(0xFFD1A7A0),
-          borderRadius: BorderRadius.circular(32),
-        ),
-        height: 500,
-        width: 1028,
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Color(0xFFD1A7A0),
+              borderRadius: BorderRadius.circular(32),
+            ),
+            height: 500,
+            width: 1028,
+          ),
+          Positioned(
+              top: 25,
+              right: 0,
+              child: Container(
+                  alignment: Alignment.centerRight,
+                  child: Image.asset(
+                    'image/calc.png',
+                    height: 450,
+                    width: 400,
+                  )))
+        ],
       ),
     );
   }
