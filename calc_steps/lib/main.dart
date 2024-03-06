2670ddb6e1da172ec935616ea660783fd6e4443e
Hazrel Idlan <ihazrel@gmail.com>
2024-03-06 22:07:07 +0800

    multiple patch
    1. add image for 570MS calculator
    2. lock app size to specific width and height

--- calc_steps/lib/main.dart
+++ calc_steps/lib/main.dart
@@ -1,7 +1,18 @@
+import 'dart:io';
+
 import 'package:calc_steps/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isIOS) {
+    setWindowTitle('Calculator Step');
+    setWindowMaxSize(const Size(1280, 720));
+    setWindowMinSize(const Size(1280, 720));
+  }
+
   runApp(const MainApp());
 }
 
